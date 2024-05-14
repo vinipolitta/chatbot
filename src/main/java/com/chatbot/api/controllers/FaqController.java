3f2bb07146666927d4605ebf6d333fb6e2c9e83d
vinipolitta <vinicius.politta1@gmail.com>
2024-05-13 21:31:30 -0300

    feat: initial logic to procude anwser

--- src/main/java/com/chatbot/api/controllers/FaqController.java
+++ src/main/java/com/chatbot/api/controllers/FaqController.java
@@ -1,18 +1,23 @@
 package com.chatbot.api.controllers;
 
 import com.chatbot.api.dto.MessageRequest;
+import com.chatbot.api.dto.Messageresponse;
+import com.chatbot.api.services.FaqService;
 import com.chatbot.api.utils.FaqAnswers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
 public class FaqController {
+    @Autowired
+    private FaqService faqService;
 
     @PostMapping
-    public ResponseEntity<String> answerQuestion(@RequestBody MessageRequest request) {
-
-        FaqAnswers faqAnswers = new FaqAnswers();
-        return ResponseEntity.ok("Deu CErto");
+    public ResponseEntity<Messageresponse> answerQuestion(@RequestBody MessageRequest request) {
+        String answer = faqService.getAnswer(request.message());
+        Messageresponse response = new Messageresponse(answer);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/chatbot/api/services/FaqService.java
+++ src/main/java/com/chatbot/api/services/FaqService.java
@@ -1,8 +1,29 @@
 package com.chatbot.api.services;
 
+import com.chatbot.api.domain.FaqAnswer;
+import com.chatbot.api.utils.FaqAnswers;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class FaqService {
 
+    final private FaqAnswers faqAnswers = new FaqAnswers();
+
+    public String getAnswer(String question){
+        String[] words = question.toLowerCase().split("\\s+");
+        List wordsList = Arrays.asList(words).stream().map(String::toLowerCase).toList();
+
+        for (FaqAnswer entry : faqAnswers.getAnswers()) {
+            for (String keyword : entry.getKeywords()) {
+                if (wordsList.contains(keyword.toLowerCase())) {
+                    return entry.getAnswer();
+                }
+            }
+        }
+
+        return faqAnswers.getDefaultAnswer();
+    }
 }
